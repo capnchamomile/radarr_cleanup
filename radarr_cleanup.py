8da15dc0c35fb29197d237e542bff51c9b761749
Dan <danielwindheim@gmail.com>
2018-07-06 08:18:22 -0700

    Add files via upload

---
+++ radarr_cleanup.py
@@ -0,0 +1,13 @@
+import os
+from os import listdir, mkdir
+from os.path import isfile, join, splitext
+
+movies = # Enter the folder where your movies are
+onlyfiles = [f for f in listdir(movies) if isfile(join(movies, f))]
+
+for file in onlyfiles:    
+    if (file.endswith('avi')) or (file.endswith('m4v')) or (file.endswith('mp4')) or (file.endswith('mkv')) or (file.endswith('divx')) or (file.endswith('xlsx')):
+        mov_dir = os.path.splitext(movies + file)[0]
+        os.mkdir(mov_dir)
+        os.rename(movies + file, mov_dir + '/' + file)
+        print('Movie file {} moved to {}'.format(file, mov_dir))
